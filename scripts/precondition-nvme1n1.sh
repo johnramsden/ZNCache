8178574e6c0547c17f94a232811bfb447fb55cab
John Ramsden <john@ubc.ramsdenj.ca>
2025-04-11 20:55:54 -0700

    Precondition once manually

--- scripts/precondition-nvme1n1.sh
+++ scripts/precondition-nvme1n1.sh
@@ -12,10 +12,10 @@ NUM_ZONES=${1}
 
 sgdisk --zap-all /dev/nvme1n1
 
-fio --name=precondition --filename=/dev/nvme1n1 --direct=1 \
-    --rw=randwrite --bs=64k --size=100% --loops=2 \
-    --randrepeat=0 --ioengine=libaio \
-    --numjobs=1 --group_reporting
+# fio --name=precondition --filename=/dev/nvme1n1 --direct=1 \
+#     --rw=randwrite --bs=64k --size=100% --loops=2 \
+#     --randrepeat=0 --ioengine=libaio \
+#     --numjobs=1 --group_reporting
 
 MIB=$((NUM_ZONES * 1077 + 1))
 
